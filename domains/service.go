a6cac357aba9510b471a590be67023c4f0d3af68
DESKTOP-OFPIBGQ\Administrator <dev.aungkyawhtay@gmail.com>
2023-09-04 15:09:48 +0630

    testing

--- domains/service.go
+++ domains/service.go
@@ -1,11 +1,14 @@
 package domains
 
 import (
-	"context"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func ServiceConn(addr string) (*grpc.ClientConn, error) {
-	return grpc.DialContext(context.Background(), addr, grpc.WithInsecure(), grpc.WithBlock())
+	// return grpc.DialContext(context.Background(), addr, grpc.WithInsecure(), grpc.WithBlock())
+
+	/// Dial
+	return grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 }
